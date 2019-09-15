6f81c48c1ed705c1130c5d0f4980f857e4d005c2
Devin <devinwreeks@gmail.com>
2019-09-15 18:17:33 +1000

    first commit

---
+++ js/script.js
@@ -0,0 +1,11 @@
+
+//   document.getElementById("opencamera").addEventListener("click", function(e){
+//       localStorage.setItem("messageitem", )
+//       window.location.href="../camerapage.html"
+//   });
+
+// window.onload = function() {
+//     var getInput = prompt("Hey type something here: ");
+//     localStorage.setItem("",);
+//  }
+
